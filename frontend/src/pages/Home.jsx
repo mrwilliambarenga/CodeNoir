e4e3da4b5fcf2daba03532c3bc0af8ad3d12c42e
mrwilliambarenga <williambarenga@gmail.com>
2023-10-07 12:30:34 +0100

    Add some buttons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,13 +16,14 @@ const Home = () => {
         />
       </div>
       <div className="text-white relative z-10 max-w-[800px] w-full md:h-[35rem] sm:h-[30rem] h-[25rem] mx-auto text-center flex flex-col justify-center">
-        <h1 className="text-9xl">
-          bubble
-        </h1>
-        <h2 className="text-2xl py-5">
-            find your bubble
-        </h2>
-        
+        <h1 className="text-9xl">bubble</h1>
+        <h2 className="text-2xl py-5">find your people. find your bubble</h2>
+        <button className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#ffffff] hover:bg-[#0080FF] text-black hover:text-white transition duration-300 ease-in-out">
+          Sign Up
+        </button>
+        <button className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg text-white hover:text-black transition duration-300 ease-in-out">
+          Log in
+        </button>
       </div>
     </div>
   );
