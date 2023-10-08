1ec46fbf1a5f7bec4df5570d84c27b553e735b2e
mrwilliambarenga <williambarenga@gmail.com>
2023-10-08 15:20:23 +0100

    Redesign the pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -37,123 +37,125 @@ const Home = () => {
           loop
         />
       </div>
-      <div
-        className={
-          showHero
-            ? "fixed bottom-0 right-0 w-[100%] h-full ease-in-out duration-700"
-            : "fixed bottom-[100%] right-0 w-[100%] h-full ease-in-out duration-700"
-        }
-        // If menu is toggled, display menu, else hide menu
-      >
-        <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-          <h1 className="text-9xl font-bubble">Bubble</h1>
-          <h2 className="text-2xl py-3 font-bold">
-            Find your people. Find your bubble
-          </h2>
-          <button
-            onClick={handleShowSignUp}
-            className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#ffffff] hover:bg-[#0080FF] text-black hover:text-white transition duration-300 ease-in-out"
-          >
-            Sign Up
-          </button>
-          <button
-            onClick={handleShowLogin}
-            className="w-[200px] rounded-md font-bold mx-auto px-6 text-lg text-white hover:text-black transition duration-300 ease-in-out"
-          >
-            Log In
-          </button>
+      <div className="relative z-10 h-screen w-screen bg-gradient-to-b from-[#b9dcff]/70 to-[#0080FF]/70">
+        <div
+          className={
+            showHero
+              ? "fixed bottom-0 right-0 w-[100%] h-full ease-in-out duration-700"
+              : "fixed bottom-[100%] right-0 w-[100%] h-full ease-in-out duration-700"
+          }
+          // If menu is toggled, display menu, else hide menu
+        >
+          <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            <h1 className="text-9xl font-bubble">Bubble</h1>
+            <h2 className="text-2xl py-3 font-bold">
+              Find your people. Find your bubble
+            </h2>
+            <button
+              onClick={handleShowSignUp}
+              className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#ffffff] hover:bg-[#0080FF] text-black hover:text-white transition duration-300 ease-in-out"
+            >
+              Sign Up
+            </button>
+            <button
+              onClick={handleShowLogin}
+              className="w-[200px] rounded-md font-bold mx-auto px-6 text-lg text-white hover:text-black transition duration-300 ease-in-out"
+            >
+              Log In
+            </button>
+          </div>
         </div>
-      </div>
-      <div
-        className={
-          showLogin
-            ? "fixed right-0 bottom-0 w-[100%] h-full ease-in-out duration-700"
-            : "fixed right-[100%] bottom-0 w-[100%] h-full ease-in-out duration-700"
-        }
-        // If login is toggled, display login, else hide login
-      >
-        <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-          <h1 className="text-6xl font-bold drop-shadow-[0_5px_5px_rgb(112,183,254)]">
-            Log In
-          </h1>
-          <div className="my-5 pt-5 flex flex-col items-center justify-between text-black">
-            <input
-              type="email"
-              className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-5"
-              placeholder="Email"
-            />
-            <input
-              type="password"
-              className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-5"
-              placeholder="Password"
-            />
-            <div className="flex flex-row justify-center items-center">
-              <input size="medium" type="checkbox" />
-              <p className="text-lg ml-2 text-white font-bold">Remember me</p>
-            </div>
-            <a href="/landing">
-              <button className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#0080FF] hover:bg-[#4cfcff] text-white hover:text-black transition duration-300 ease-in-out">
-                Sign In
-              </button>
-            </a>
-            <div className="flex flex-row justify-center items-center text-center mt-4">
-              <p className="font-bold">Don't have an account?</p>
-              <button
-                onClick={handleShowSignUp}
-                className="w-[200px] rounded-md font-bold mx-auto px-6 text-lg text-white hover:text-black transition duration-300 ease-in-out"
-              >
-                Sign Up
-              </button>
+        <div
+          className={
+            showLogin
+              ? "fixed right-0 bottom-0 w-[100%] h-full ease-in-out duration-700"
+              : "fixed right-[100%] bottom-0 w-[100%] h-full ease-in-out duration-700"
+          }
+          // If login is toggled, display login, else hide login
+        >
+          <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            <h1 className="text-6xl font-bold drop-shadow-[0_5px_5px_rgb(112,183,254)]">
+              Log In
+            </h1>
+            <div className="my-5 pt-5 flex flex-col items-center justify-between text-black">
+              <input
+                type="email"
+                className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-5"
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-5"
+                placeholder="Password"
+              />
+              <div className="flex flex-row justify-center items-center">
+                <input size="medium" type="checkbox" />
+                <p className="text-lg ml-2 text-white font-bold">Remember me</p>
+              </div>
+              <a href="/landing">
+                <button className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#0080FF] hover:bg-[#4cfcff] text-white hover:text-black transition duration-300 ease-in-out">
+                  Sign In
+                </button>
+              </a>
+              <div className="flex flex-row justify-center items-center text-center mt-4">
+                <p className="font-bold">Don't have an account?</p>
+                <button
+                  onClick={handleShowSignUp}
+                  className="w-[200px] rounded-md font-bold mx-auto px-6 text-lg text-white hover:text-black transition duration-300 ease-in-out"
+                >
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className={
-          showSignUp
-            ? "fixed left-0 bottom-0 w-[100%] h-full ease-in-out duration-700"
-            : "fixed left-[100%] bottom-0 w-[100%] h-full ease-in-out duration-700"
-        }
-        // If signup is toggled, display signup, else hide signup
-      >
-        <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-          <h1 className="text-6xl font-bold drop-shadow-[0_5px_5px_rgb(112,183,254)]">
-            Sign Up
-          </h1>
-          <div className="my-5 pt-5 flex flex-col items-center justify-between text-black">
-            <input
-              type="text"
-              className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold mt-3 mb-3"
-              placeholder="First Name"
-            />
-            <input
-              type="text"
-              className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-3"
-              placeholder="Last Name"
-            />
-            <input
-              type="email"
-              className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-3"
-              placeholder="Email"
-            />
-            <input
-              type="password"
-              className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold mt-3 mb-3"
-              placeholder="Password"
-            />
-            <a href="/landing">
-              <button className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#0080FF] hover:bg-[#4cfcff] text-white hover:text-black transition duration-300 ease-in-out">
-                Register
-              </button>
-            </a>
-            <div className="flex flex-row justify-center text-center items-center">
-              <p className="font-bold">Already have an account?</p>
-              <button
-                onClick={handleShowLogin}
-                className="w-[200px] rounded-md font-bold mx-auto px-6 text-lg text-white hover:text-black transition duration-300 ease-in-out"
-              >
-                Log In
-              </button>
+        <div
+          className={
+            showSignUp
+              ? "fixed left-0 bottom-0 w-[100%] h-full ease-in-out duration-700"
+              : "fixed left-[100%] bottom-0 w-[100%] h-full ease-in-out duration-700"
+          }
+          // If signup is toggled, display signup, else hide signup
+        >
+          <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            <h1 className="text-6xl font-bold drop-shadow-[0_5px_5px_rgb(112,183,254)]">
+              Sign Up
+            </h1>
+            <div className="my-5 pt-5 flex flex-col items-center justify-between text-black">
+              <input
+                type="text"
+                className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold mt-3 mb-3"
+                placeholder="First Name"
+              />
+              <input
+                type="text"
+                className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-3"
+                placeholder="Last Name"
+              />
+              <input
+                type="email"
+                className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold my-3"
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                className="bg-white w-[45rem] rounded-full py-4 px-2 text-center font-bold mt-3 mb-3"
+                placeholder="Password"
+              />
+              <a href="/landing">
+                <button className="w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3 text-lg bg-[#0080FF] hover:bg-[#4cfcff] text-white hover:text-black transition duration-300 ease-in-out">
+                  Register
+                </button>
+              </a>
+              <div className="flex flex-row justify-center text-center items-center">
+                <p className="font-bold">Already have an account?</p>
+                <button
+                  onClick={handleShowLogin}
+                  className="w-[200px] rounded-md font-bold mx-auto px-6 text-lg text-white hover:text-black transition duration-300 ease-in-out"
+                >
+                  Log In
+                </button>
+              </div>
             </div>
           </div>
         </div>
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,65 +2,78 @@ import React from "react";
 
 import Typed from "react-typed";
 
+import bubbles from "../assets/bubbles.mp4";
 import jigsaw from "../assets/jigsaw.png";
 import uni from "../assets/uni.png";
 
 const Landing = () => {
   return (
-    <div className="h-screen text-white bg-gradient-to-b from-[#b9dcff] to-[#0080FF]">
-      <div className="grid grid-rows-none grid-cols-3">
-        <div className="col-span-2">
-          <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-            <h1 className="text-5xl pb-5 font-bold drop-shadow-[0_5px_5px_rgb(100,177,253)]">
-              Take your
-            </h1>
-            <h1 className="text-7xl pb-14 font-bubble drop-shadow-[0_5px_5px_rgb(100,177,253)]">
-              Psychometric Test
-            </h1>
-            <div className="grid grid-rows-none grid-cols-3">
-              <div className="col-span-2 flex flex-col justify-center">
-                <h2 className="text-3xl py-4 font-semibold">
-                  Find your personality type
-                </h2>
-                <div className="flex justify-center items-center self-center my-5">
-                  <h2 className="text-3xl py-10 mr-2 font-semibold">
-                    Match with 
+    <div>
+      <div className="absolute z-0 top-0 w-full h-full">
+        <video
+          className="object-cover w-[100%] h-[100%]"
+          src={bubbles}
+          type="video/mp4"
+          autoPlay
+          muted
+          loop
+        />
+      </div>
+      <div className="h-screen relative z-10 text-white bg-gradient-to-b from-[#b9dcff]/70 to-[#0080FF]/70">
+        <div className="grid grid-rows-none grid-cols-3">
+          <div className="col-span-2">
+            <div className="text-white relative z-10 max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+              <h1 className="text-5xl pb-5 font-bold drop-shadow-[0_5px_5px_rgb(100,177,253)]">
+                Take your
+              </h1>
+              <h1 className="text-7xl pb-14 font-bubble drop-shadow-[0_5px_5px_rgb(100,177,253)]">
+                Psychometric Test
+              </h1>
+              <div className="grid grid-rows-none grid-cols-3">
+                <div className="col-span-2 flex flex-col justify-center">
+                  <h2 className="text-3xl py-4 font-semibold">
+                    Find your personality type
                   </h2>
-                  <Typed
-                    className="text-3xl py-10 font-bold"
-                    strings={[
-                      "uni societies",
-                      "friends",
-                      "events",
-                      "clubs",
-                      "teams",
-                      "students",
-                    ]}
-                    typeSpeed={100}
-                    backSpeed={100}
-                    loop
-                  />
+                  <div className="flex justify-center items-center self-center my-5">
+                    <h2 className="text-3xl py-10 mr-2 font-semibold">
+                      Match with
+                    </h2>
+                    <Typed
+                      className="text-3xl py-10 font-bold"
+                      strings={[
+                        "uni societies",
+                        "friends",
+                        "events",
+                        "clubs",
+                        "teams",
+                        "students",
+                      ]}
+                      typeSpeed={100}
+                      backSpeed={100}
+                      loop
+                    />
+                  </div>
+                  <h2 className="text-3xl py-4 font-semibold">
+                    Find your bubble
+                  </h2>
                 </div>
-                <h2 className="text-3xl py-4 font-semibold">
-                  Find your bubble
-                </h2>
-              </div>
-              <div className="col-span-1">
-                <img className="w-[250px] mx-auto pb-5" src={jigsaw} alt="" />
-                <img className="w-[250px] mx-auto pt-5" src={uni} alt="" />
+                <div className="col-span-1">
+                  <img className="w-[250px] mx-auto pb-5" src={jigsaw} alt="" />
+                  <img className="w-[250px] mx-auto pt-5" src={uni} alt="" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-span-1">
-          <a
-            href="/test"
-            className="max-w-[800px] w-[200px] h-screen ml-14 text-center flex flex-col justify-center"
-          >
-            <button className="text-center rounded-md font-bold mx-auto my-6 px-16 py-6 text-5xl bg-[#ffffff] hover:bg-[#0080FF] text-black hover:text-white transition duration-300 ease-in-out drop-shadow-[0_5px_5px_rgb(100,177,253)]">
-              Begin
-            </button>
-          </a>
+          <div className="col-span-1">
+            <a
+              href="/test"
+              className="max-w-[800px] w-[200px] h-screen ml-14 text-center flex flex-col justify-center"
+            >
+              <button className="text-center rounded-md font-bold mx-auto my-6 px-16 py-6 text-5xl bg-[#ffffff] hover:bg-[#0080FF] text-black hover:text-white transition duration-300 ease-in-out drop-shadow-[0_5px_5px_rgb(100,177,253)]">
+                Begin
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
